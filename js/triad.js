47546157c4d4130d3a4ff6cd3f9ccd886c3a26ae
Victor Giovannoni Vernalha <vernalhav@usp.br>
2020-02-22 00:53:08 -0300

    Implemented server sending files

--- js/triad.js
+++ js/triad.js
@@ -1,9 +1,12 @@
 
+let isSaved = true;         // Whether document is up to date
 let removing = false;       // Whether removing notes or not
+
 let notesJSON = {};         // Object containing all notes
 let currentNoteStack = [];  // Path taken from root note to current note
 
 const FILENAME = "notes.json";
+const MSG_SUCCESS = "OK";
 
 // Future features
 //   Edit notes
@@ -18,53 +21,8 @@ function main(){
             saveFile();
         }
     }, false);
-}
 
-
-function saveFile(){
-    const requestJSON = {
-        "function" : "saveFile",
-        "args" : {
-            "name" : FILENAME,
-            "json" : notesJSON
-        }
-    };
-
-    sendJSON(requestJSON, ()=>console.log("File saved."));
-}
-
-
-function ignoreReply(){}
-
-
-// TODO: write json to screen
-function setupPreviousFile(json){
-    notesJSON = JSON.parse(this.response);
-    if (Object.keys(notesJSON).length == 0) return;
-}
-
-/*
-    Sends a json object to the server via a
-    POST request. Calls callback when reply
-    is available
-*/
-function sendJSON(json, callback=ignoreReply){
-    const messageString = JSON.stringify(json);
-
-    var xmlRequest = new XMLHttpRequest();
-    xmlRequest.addEventListener("load", callback); // Calls callback when response is complete and reply available
-    xmlRequest.open("POST", "/");
-    xmlRequest.setRequestHeader("Content-Type", "application/json"); // Needs this so that express.json() recognizes it
-    
-    xmlRequest.send(messageString);
-}
-
-
-// TODO: Traverse through the tree or remove text
-function textClicked(element){
-    let text = element.target.childNodes[0].nodeValue;
-    console.log(text);
-
+    requestNotes(FILENAME);
 }
 
 
@@ -88,6 +46,78 @@ function addNote(){
     notesList.appendChild(node);
 
     document.getElementById("note_input").value = "";
+    isSaved = false;
+}
+
+
+function requestNotes(name){
+    const requestJSON = {
+        "function" : "getFile",
+        "args" : {
+            "name" : FILENAME
+        }
+    };
+
+    sendJSON(requestJSON, function(event){
+        console.log(event);
+        let response = JSON.parse(event.target.response);
+        if (response.status != MSG_SUCCESS) return;
+
+        notesJSON = response.file;
+        displayNotes(notesJSON);
+    });
+}
+
+
+function saveFile(){
+    const requestJSON = {
+        "function" : "saveFile",
+        "args" : {
+            "name" : FILENAME,
+            "json" : notesJSON
+        }
+    };
+
+    sendJSON(requestJSON, ()=>{
+        isSaved = true;
+        console.log("File saved.")
+    });
+}
+
+
+function ignoreReply(){}
+
+
+// TODO: write json to screen
+function setupPreviousFile(json){
+    if (Object.keys(notesJSON).length == 0) return;
+}
+
+/*
+    Sends a json object to the server via a
+    POST request. Calls callback when reply
+    is available.
+    NOTE: Callback should not be an arrow
+          function because it overwrites
+          'this' to a global scope.
+*/
+function sendJSON(json, callback=ignoreReply){
+    const messageString = JSON.stringify(json);
+
+    var xmlRequest = new XMLHttpRequest();
+    xmlRequest.addEventListener("load", callback); // Calls callback when response is complete and reply available
+    xmlRequest.open("POST", "/");
+    xmlRequest.setRequestHeader("Content-Type", "application/json"); // Needs this so that express.json() recognizes it
+    
+    xmlRequest.send(messageString);
+}
+
+
+// TODO: Traverse through the tree or remove text
+function textClicked(element){
+    let text = element.target.childNodes[0].nodeValue;
+    console.log(text);
+
 }
 
 
@@ -109,6 +139,7 @@ function toggleRemoving(){
     json: json file whose keys are notes
 */
 function displayNotes(json){
+    console.log(json);
     let notesList = document.getElementById("notes_list");
      
     while (notesList.hasChildNodes())
@@ -117,5 +148,3 @@ function displayNotes(json){
 
 
 main();
-
-
